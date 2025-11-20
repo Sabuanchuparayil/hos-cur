f911c1c8105948e575966f2ae7a6993f0aa83769
Sabu J <mail@jsabu.com>
2025-11-20 10:52:03 +0400

    Cleanup: removed duplicate HoS project and DS_Store files

--- untitled folder/HoS/data/reasons.ts
+++
@@ -1,9 +0,0 @@
-import { ReturnReason } from '../types';
-
-export const RETURN_REASONS: ReturnReason[] = [
-    { code: 'WRONG_ITEM', description: 'Received the wrong item' },
-    { code: 'DAMAGED', description: 'Item arrived damaged' },
-    { code: 'UNWANTED', description: 'No longer want/need the item' },
-    { code: 'WRONG_SIZE', description: 'Item is the wrong size/fit' },
-    { code: 'OTHER', description: 'Other (please specify)' },
-];
