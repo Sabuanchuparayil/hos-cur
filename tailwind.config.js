6531defcffa6bf806ad1be5ac8f356dba99472a2
Sabu J <mail@jsabu.com>
2025-11-21 09:27:56 +0400

    fix: tailwind safelist for dynamic themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,20 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  safelist: [
+    {
+      pattern: /theme-.+/,
+    },
+    {
+      pattern: /bg-.+/,
+    },
+    {
+      pattern: /text-.+/,
+    },
+    {
+      pattern: /border-.+/,
+    },
+  ],
   theme: {
     extend: {},
   },
